2dd8da812439efa4fed0dcdced5dfa10692e3dae
Lebiru <bilal.akabi@gmail.com>
2021-10-10 13:17:45 -0400

    fix spacing

--- Monitor/Startup.cs
+++ Monitor/Startup.cs
@@ -60,13 +60,13 @@ namespace Monitor
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
-                    {
-                    endpoints.MapControllerRoute(
-                            name: "default",
-                            pattern: "{controller=Home}/{action=Index}/{id?}");
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
 
-                    endpoints.MapHealthChecksUI();
-                    });
+                endpoints.MapHealthChecksUI();
+            });
         }
     }
 }
