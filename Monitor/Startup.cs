893679aa04aae4378ea04da8135eb906e43320c3
Lebiru <bilal.akabi@gmail.com>
2021-10-10 13:18:56 -0400

    removal of extra line

--- Monitor/Startup.cs
+++ Monitor/Startup.cs
@@ -64,7 +64,6 @@ namespace Monitor
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
-
                 endpoints.MapHealthChecksUI();
             });
         }
